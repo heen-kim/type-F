6b7ed63b7a6aa674dc51766cf893aa6cf2b7e3ea
unknown <heeeun9529@gmail.com>
2026-03-27 22:45:31 +0900

    슬라이드 완성

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+
+  // 2차 메뉴
   $('.menu>ul>li').mouseover(function(){
     $(this).find('.menu2').stop().slideDown(500);
   }).mouseout(function(){
     $(this).find('.menu2').stop().slideUp(500);
   });
+
+  // 슬라이드
+  $('.slide li:gt(0)').hide();
+
+  setInterval(function(){
+    $('.slide li:first-child').fadeOut()
+    .next('li').fadeIn().end().appendTo('.slide ul');
+  },3000);
 });
