a9cba207cc7bfb0a37a9d010748dfafb6fd2b3ef
unknown <heeeun9529@gmail.com>
2026-03-27 20:40:45 +0900

    헤더 완성

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+  $('.menu>ul>li').mouseover(function(){
+    $(this).find('.menu2').stop().slideDown(500);
+  }).mouseout(function(){
+    $(this).find('.menu2').stop().slideUp(500);
+  });
 });
